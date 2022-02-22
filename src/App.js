091de5a428f27efd9f420f5aef6608639ed9d011
Levon Midoyan <levonmidoyan.midoyan@gmail.com>
2022-02-22 21:15:19 +0400

    Footer html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
         <Main/>
+        <Footer/>
     </div>
   );
 }
