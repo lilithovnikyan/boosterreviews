939c7c70a8df4424d269e90f55557181b9de7d25
vaawebdev <vaawebdev@gmail.com>
2022-02-22 21:02:35 +0400

    navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Nav from "./nav/Nav";
 
 export default function Header() {
     return (
-        <div>Header</div>
+        <div>
+
+            <Nav/>
+        </div>
     )
 }
